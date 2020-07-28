af66ba8e2b69ee978d780d4eeda9f80a4dc13219
Jamuna <59509012+cherry-pixel@users.noreply.github.com>
2020-07-28 10:43:51 +0530

    Update Report.sol

--- Blockchain/Day 4/Assignment/Report.sol
+++ Blockchain/Day 4/Assignment/Report.sol
@@ -3,33 +3,47 @@ pragma solidity ^0.4.17 < 0.6.12;
 contract ReportCard{
     
     string name;
-    uint Id;
+    uint roll;
     int maths;
     int science;
     int english;
+    int social;
+    int batch;
     string result;
     
-    function Report(string studname,uint studId,int math,int sci,int eng,int res) public{
+    function Report(string studname,uint studId,int math,int sci,int eng,int soc,int grp,int res) public{
+       
        name = studname;
-       Id = studId;
+       roll = studId;
        maths = math;
        science = sci;
        english = eng;
+       social = soc;
+       batch = grp;
        
-       res = (math+sci+eng)/3;
+       res = (math+sci+eng+soc)/4;
        if(res>50)
             result="pass";
        else
             result="fail";
     }
     
-    function changeMarks(int math,int sci,int eng,int) public{
+    function changeMarks(int math,int sci,int eng,int soc,int res) public{
+        
         maths = math;
         science = sci;
         english = eng;
+        social = soc;
+        res = (math+sci+eng+soc)/4;
+        if(res>50)
+            result="pass";
+        else
+            result="fail";
     }
-    function getReportCard() public view returns(string,uint,int,int,int,string){
-        return(name,Id,maths,science,english,result);
+    
+    function getReportCard() public view returns(string,uint,int,int,int,int,int,string){
+        
+        return(name,roll,maths,science,english,social,batch,result);
     }
     
 }
